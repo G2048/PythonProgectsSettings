efba87b523154a34e2d0b5e615b5e0c5faf83455
IX <borntokill.ix@gmail.com>
2024-12-25 17:47:08 +0700

    feat(services): add base class for communication remote http api

---
+++ app/services/api/base.py
@@ -0,0 +1,55 @@
+import json
+from json import JSONDecodeError
+from typing import TypeAlias
+
+from httpx import AsyncClient
+
+from app.configs import get_logger
+
+logger = get_logger()
+
+Json: TypeAlias = str
+
+
+class BaseApi:
+    def __init__(self, url):
+        self.HEADERS = {'Content-Type': 'application/json'}
+        self.URL = url
+        self._status_code = None
+
+    @staticmethod
+    def _validateJson(jsondata):
+        try:
+            return jsondata()
+        except JSONDecodeError:
+            return None
+
+    async def _request(self, url, query_params=None, method='GET', **kwargs):
+        self.URL += url
+        logger.debug(f'{url}, {query_params=}, {self.HEADERS=}')
+
+        async with AsyncClient(follow_redirects=True) as client:
+            self._response = await client.request(
+                method=method, url=self.URL, params=query_params, headers=self.HEADERS, **kwargs
+            )
+
+            self.status_code = self._response.status_code
+            logger.debug(self._response.status_code)
+            logger.debug(self._response.text)
+
+            if self._response.status_code < 300:
+                response_json = self._validateJson(self._response.json)
+                if response_json:
+                    return response_json
+
+    @property
+    def status_code(self) -> int | None:
+        return self._status_code
+
+    @status_code.setter
+    def status_code(self, value) -> None:
+        self._status_code = value
+
+    @staticmethod
+    def serialize(data: Json) -> dict:
+        return json.loads(data)
