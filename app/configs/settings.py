d5cf4791c7014d28d8a6a171802d064adfbf86a9
IX <borntokill.ix@gmail.com>
2024-12-25 10:56:38 +0700

    fix(settings): not found LogSettings module

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pydantic import computed_field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from .LogSettings import set_appname, set_appversion, set_debug_level
+from .log_settings import set_appname, set_appversion, set_debug_level
 
 load_dotenv()
 
