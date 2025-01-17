3288f9a554716696b00a269cc2a2f84a3336c505
IX <borntokill.ix@gmail.com>
2025-01-18 00:42:56 +0700

    ref(api): add more type hinting

--- app/services/api/base.py
+++ app/services/api/base.py
@@ -1,6 +1,6 @@
 import json
 from json import JSONDecodeError
-from typing import TypeAlias
+from typing import Any, Callable, TypeAlias
 
 from httpx import AsyncClient
 
@@ -14,13 +14,13 @@ Json: TypeAlias = str
 class BaseApi:
     logger = get_logger()
 
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.HEADERS = {'Content-Type': 'application/json'}
-        self.URL = url
-        self._status_code = None
+        self.URL: str = url
+        self._status_code: int = -1
 
     @staticmethod
-    def _validateJson(jsondata):
+    def _validateJson(jsondata: Callable[[], dict[str, str]]) -> dict[str, str] | None:
         try:
             return jsondata()
         except JSONDecodeError:
@@ -29,8 +29,8 @@ class BaseApi:
     def _concat_url(self, url: str) -> str:
         return self.URL + url
 
-    async def _request(self, url, query_params=None, method='GET', **kwargs):
-        url = _concat_url(url)
+    async def _request(self, url: str, query_params=None, method='GET', **kwargs):
+        url = self._concat_url(url)
         self.logger.debug(f'{url}, {query_params=}, {self.HEADERS=}')
 
         async with AsyncClient(follow_redirects=True) as client:
@@ -51,13 +51,13 @@ class BaseApi:
                 raise HTTPException(self._response.status_code, self._response.text)
 
     @property
-    def status_code(self) -> int | None:
+    def status_code(self) -> int:
         return self._status_code
 
     @status_code.setter
-    def status_code(self, value) -> None:
+    def status_code(self, value: int) -> None:
         self._status_code = value
 
     @staticmethod
-    def serialize(data: Json) -> dict:
+    def serialize(data: Json) -> dict[Any, Any]:
         return json.loads(data)
