b99ab16a9404f4235135ef34daaf7478513b5e13
IX <borntokill.ix@gmail.com>
2025-09-12 21:39:18 +0700

    feat(app/configs/pyproject.py): add parsing uv section in pyproject.toml

--- app/configs/pyproject.py
+++ app/configs/pyproject.py
@@ -1,27 +1,40 @@
 import tomllib
+from enum import StrEnum
+
+
+class Managers(StrEnum):
+    poetry = 'tool.poetry'
+    uv = 'project'
 
 
 class _ParserPyproject:
-    def __init__(self, pyproject_toml='pyproject.toml'):
-        with open('pyproject.toml', 'rb') as f:
+    def __init__(self, manager: Managers, pyproject_toml: str = 'pyproject.toml'):
+        with open(pyproject_toml, 'rb') as f:
             self._pyproject_toml = tomllib.load(f)
         self._tool = self._pyproject_toml.get('tool', {})
-        self._poetry = self._tool.get('poetry', {})
 
-        self.name = self._poetry.get('name', {})
-        self.version = self._poetry.get('version')
+        _prev_manager = None
+        for key in manager.value.split('.'):
+            self._manager = self._pyproject_toml.get(key, {})
+            if self._manager:
+                _prev_manager = self._manager
+            else:
+                self._manager = _prev_manager.get(key, {})
+
+        self.name = self._manager.get('name', {})
+        self.version = self._manager.get('version')
 
 
 class ParserPyproject:
     __slots__ = ()
-    _parser = _ParserPyproject()
+    _parser = _ParserPyproject(Managers.uv)
     name = _parser.name
     version = _parser.version
 
 
 def test_parser_pyproject():
-    parser = _ParserPyproject()
-    assert parser.version == '0.1.0'
+    parser = _ParserPyproject(Managers.uv)
+    assert parser.version == '0.2.0', f'Current version is {parser.version}'
     assert parser.name == 'Audio Tools'
     assert isinstance(parser._tool, dict)
     print(f'{parser._tool=}')
